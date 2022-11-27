dafb18ef2beeaaf7eac0ec6c93a78f8888f6ff3a
Zach Xu <zach.popping@gmail.com>
2022-11-27 12:44:50 -0500

    login and signup UI completed

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import axios from 'axios';
+import { baseURL } from '../../config/env'
 
 export default function Login() {
+
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    })
+
+    const login = async (e) => {
+        e.preventDefault()
+        e.preventDefault()
+        const { data } = await axios.post(`${baseURL}/api/users/login`, user)
+        console.log(data);
+    }
+
     return (
-        <div>Login</div>
+        <Box component="form" sx={{ display: 'flex', flexDirection: 'column' }} onSubmit={login}>
+
+            <TextField
+                type='email'
+                required
+                label="Email"
+                sx={{ minWidth: '400px', margin: '10px 0' }}
+                value={user.email}
+                onChange={e => { setUser({ ...user, email: e.target.value }) }}
+            />
+            <TextField
+                required
+                type="password"
+                label="Password"
+                sx={{ minWidth: '400px', margin: '10px 0' }}
+                onChange={e => { setUser({ ...user, password: e.target.value }) }}
+            />
+            <Button type="submit" variant="contained" sx={{ mt: '10px', fontSize: '18px' }}>Log In</Button>
+        </Box>
     )
 }
--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import axios from 'axios';
+import { baseURL } from '../../config/env'
 
 export default function Signup() {
+    const [user, setUser] = useState({
+        username: '',
+        email: '',
+        password: ''
+    })
+
+    const signup = async (e) => {
+        e.preventDefault()
+        const { data } = await axios.post(`${baseURL}/api/users`, user)
+        console.log(data);
+    }
+
     return (
-        <div>Signup</div>
+        <Box component="form" sx={{ display: 'flex', flexDirection: 'column' }} onSubmit={signup}>
+            <TextField
+                required
+                label="Username"
+                sx={{ minWidth: '400px', margin: '10px 0' }}
+                value={user.username}
+                onChange={e => { setUser({ ...user, username: e.target.value }) }}
+            />
+            <TextField
+                required
+                type='email'
+                label="Email"
+                sx={{ minWidth: '400px', margin: '10px 0' }}
+                value={user.email}
+                onChange={e => { setUser({ ...user, email: e.target.value }) }}
+            />
+            <TextField
+                required
+                type="password"
+                label="Password"
+                sx={{ minWidth: '400px', margin: '10px 0' }}
+                onChange={e => { setUser({ ...user, password: e.target.value }) }}
+            />
+            <Button type="submit" variant="contained" sx={{ mt: '10px', fontSize: '18px' }}>Sign Up</Button>
+        </Box>
     )
 }
